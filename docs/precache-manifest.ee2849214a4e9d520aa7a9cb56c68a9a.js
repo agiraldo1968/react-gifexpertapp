5576b7a826a2303d981d2b212f23a0cb34dc3e65
Andres Giraldo <afgir@hotmail.com>
2020-08-24 11:23:12 -0500

    docs agregado

---
+++ docs/precache-manifest.ee2849214a4e9d520aa7a9cb56c68a9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99cd8e6b3a81b381eff38f7174d7c530",
+    "url": "/index.html"
+  },
+  {
+    "revision": "630b50d1b7634c32bf5a",
+    "url": "/static/css/main.c57d29c3.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "630b50d1b7634c32bf5a",
+    "url": "/static/js/main.210d834a.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
